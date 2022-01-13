dea6b84c40847e12b230fecace5a9ddd9a63376f
LOSAFABIO <fabio.losa95@gmail.com>
2022-01-13 15:01:17 +0100

    Fatto

---
+++ main.js
@@ -0,0 +1,12 @@
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+// Bonus:
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+let myVue = new Vue({
+    el   : `.container`,
+    data : {
+        testo    : `Ciao programmatore`,
+        immagine : `https://www.a-sapiens.it/wp-content/uploads/freshizer/0b5b493b665a0f01db6d0842abe9417f_corso-programmatore-768x510-640x342-1-640-c-90.jpg`
+    },
+})
